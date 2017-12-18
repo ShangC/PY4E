0ed3c50d5b32b4bf095d79e54cfbebc3fbb6d47c
ShangC <cstomjm211@hotmail.com>
2017-12-18 18:44:39 -0500

    Only type the actual command to run the python file in vs code terminal can ignore the path issue

--- Course3/Chapter11Assignment/11.py
+++ Course3/Chapter11Assignment/11.py
@@ -2,7 +2,8 @@ import re
 
 #This is the only work-around to make sure VS Code can read the file in terminal directly. 
 #Remember to remove the directory when uploading to assignment because it will cause FileNotFoundError
-fhandle = open('c:/Users/AdminWLGlobal/Desktop/py4e/Course3/Chapter11Assignment/regex_sum_44132.txt')
+#c:/Users/AdminWLGlobal/Desktop/py4e/Course3/Chapter11Assignment/
+fhandle = open('regex_sum_44132.txt')
 
 for line in fhandle:
     line = line.rstrip()
