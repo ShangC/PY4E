4dda4e8300bd9be18f7d4c1320b03a76a946caf4
Shang Chen <shangchen@Shangs-MacBook-Pro.local>
2017-12-18 02:09:16 -0500

    10-2 assignment revised

--- Course2/Chapter10Assignment/10_2.py
+++ Course2/Chapter10Assignment/10_2.py
@@ -1,4 +1,21 @@
-#abc
-#bbc
-#ccc
-#dcc
+fhandle = open('mbox-short.txt')
+
+hour_histogram = {}
+tuple_list = []
+
+
+for line in fhandle:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    time = line.split()[5]
+    hour = time.split(':')[0]
+    hour_histogram[hour] = hour_histogram.get(hour, 0) + 1
+
+for key in hour_histogram:
+    tuple_list.append((key, hour_histogram[key])
+
+
+
+for item in list_sort:
+    print(item)
