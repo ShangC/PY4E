b1d8343f7b5c4632a502ab51241d4efe430e7fcc
ShangC <cstomjm211@hotmail.com>
2017-12-18 18:45:49 -0500

    add comment to notify the path issue in python file

--- Course3/Chapter11Assignment/11.py
+++ Course3/Chapter11Assignment/11.py
@@ -1,8 +1,9 @@
 import re
 
-#This is the only work-around to make sure VS Code can read the file in terminal directly. 
-#Remember to remove the directory when uploading to assignment because it will cause FileNotFoundError
-#c:/Users/AdminWLGlobal/Desktop/py4e/Course3/Chapter11Assignment/
+# This is the only work-around to make sure VS Code can read the file in terminal directly.
+# Remember to remove the directory when uploading to assignment because it will cause FileNotFoundError
+# c:/Users/AdminWLGlobal/Desktop/py4e/Course3/Chapter11Assignment/
+# otherwise simply type the python command in below terminal will bypass the path problem
 fhandle = open('regex_sum_44132.txt')
 
 for line in fhandle:
