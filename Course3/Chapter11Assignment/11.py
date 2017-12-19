40d607408b08212b956e5712de76d0fefae8f966
ShangC <cstomjm211@hotmail.com>
2017-12-19 13:38:30 -0500

    Finished chapter 11 assignment

--- Course3/Chapter11Assignment/11.py
+++ Course3/Chapter11Assignment/11.py
@@ -5,8 +5,12 @@ import re
 # c:/Users/AdminWLGlobal/Desktop/py4e/Course3/Chapter11Assignment/
 # otherwise simply type the python command in below terminal will bypass the path problem
 fhandle = open('regex_sum_44132.txt')
-
+total = 0
 for line in fhandle:
     line = line.rstrip()
     numberInLine = re.findall('[0-9]+', line)
-    print(numberInLine)
+    #print(numberInLine)
+    for num in numberInLine:
+        total += int(num)
+
+print(total)
