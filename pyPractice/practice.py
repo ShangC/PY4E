52c2ff27856415a77485395321376b4b30ed67a3
ShangC <cstomjm211@hotmail.com>
2017-12-19 18:41:47 -0500

    practice code

--- pyPractice/practice.py
+++ pyPractice/practice.py
@@ -1,23 +1,26 @@
-import re
+#Chapter 11 Exercise
+# import re
 
-x = 'From 90:12:22 shang.chen@wlglobalcorp.com.cn Sat Jan 4 09:14:22 2015'
+# x = 'From 90:12:22 shang.chen@wlglobalcorp.com.cn Sat Jan 4 09:14:22 2015'
 
-y = re.findall('\S+@\S+', x)
-print(y)
+# y = re.findall('\S+@\S+', x)
+# print(y)
 
-z = re.findall('\S?@\S?', x)
-print(z)
+# z = re.findall('\S?@\S?', x)
+# print(z)
 
-zz = re.findall('[a-z0-9]', x)
-print(zz)
-print(type(zz))
+# zz = re.findall('[a-z0-9]', x)
+# print(zz)
+# print(type(zz))
 
-zzz = re.findall('\S+?@\S+', x)
-print(zzz)
+# zzz = re.findall('\S+?@\S+', x)
+# print(zzz)
 
-zzzz = re.findall('@(\S+)', x)
-print(zzzz)
+# zzzz = re.findall('@(\S+)', x)
+# print(zzzz)
 
-a = 'From: Using the : Character'
-b = re.findall('^F.+:', a)
-print(b)
+# a = 'From: Using the : Character'
+# b = re.findall('^F.+:', a)
+# print(b)
+
+
