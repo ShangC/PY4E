e015d62e42e9a8a1c7af9e03e67e91f808131f43
ShangC <cstomjm211@hotmail.com>
2018-01-30 19:09:05 -0500

    Week4 assignment 1 practice

--- pyPractice/practice.py
+++ pyPractice/practice.py
@@ -1,4 +1,4 @@
-#Chapter 11 Exercise
+# Chapter 11 Exercise
 # import re
 
 # x = 'From 90:12:22 shang.chen@wlglobalcorp.com.cn Sat Jan 4 09:14:22 2015'
@@ -24,6 +24,71 @@
 # print(b)
 
 ############################################################################
-#Chapter 12 Exercise
+# Chapter 12 Exercise
 # this is a test
 # this is a test from vscode with git file downloaded from github desktop
+#
+# import urllib.request
+# import urllib.parse
+# import urllib.error
+# fhand = urllib.request.urlopen('https://en.wikipedia.org/wiki/China')
+# for line in fhand:
+#     print(line.decode().strip())
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/comments_42.html'
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# # Retrieve all of the anchor tags
+# spans = soup('span')
+# numbers = []
+# for span in spans:
+#     numbers.append(int(span.string))
+#     print(span)
+#     print(span.string)
+# print(numbers)
+# print(sum(numbers))
+
+# Chapter 12 Exercise
+# this is a test
+# this is a test from vscode with git file downloaded from github desktop
+#
+# import urllib.request
+# import urllib.parse
+# import urllib.error
+# fhand = urllib.request.urlopen('https://en.wikipedia.org/wiki/China')
+# for line in fhand:
+#     print(line.decode().strip())
+
+# import urllib.request, urllib.parse, urllib.error
+# from bs4 import BeautifulSoup
+# import re
+# import ssl
+
+# # Ignore SSL certificate errors
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
+
+# url = 'http://py4e-data.dr-chuck.net/comments_42.html'
+# html = urllib.request.urlopen(url, context=ctx).read()
+# soup = BeautifulSoup(html, 'html.parser')
+
+# # Retrieve all of the anchor tags
+# spans = soup('span')
+# numLst = []
+# for span in spans:
+#     print(span)
+#     x = re.findall('[0-9]+', int(span.string))
+#     numLst.append(x)
+# print(numLst)
+# print(sum(numLst))
